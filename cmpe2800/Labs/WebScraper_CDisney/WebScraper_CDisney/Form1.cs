377628b887234449064513391b0ceeac44bc1d82
1201_cdisney1 <1201_cdisney1@319ddbfa-8900-c54b-9324-0d5e74af71d5>
2022-03-01 20:19:30 +0000

    Initial commit / Form designed

---
+++ cmpe2800/Labs/WebScraper_CDisney/WebScraper_CDisney/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebScraper_CDisney
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Labels
+            UI_Label_URLBox.Text = "URL:";
+            UI_Label_GridView.Text = "Loaded images:";
+            UI_Label_Listbox.Text = "Notifications:";
+
+            //Load Button
+            UI_Button_Load.Text = "Load";
+            UI_Button_Load.Click += UI_Button_Load_Click;
+
+            //Save Button
+            UI_Button_Save.Text = "Save";
+            UI_Button_Save.Click += UI_Button_Save_Click;
+
+            //Cancel Button
+            UI_Button_Cancel.Text = "Cancel";
+            UI_Button_Cancel.Click += UI_Button_Cancel_Click;
+        }
+
+        //------------------Form Methods------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //------------------Cancel Loading------------------
+        private void UI_Button_Cancel_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        //------------------Save Images------------------
+        private void UI_Button_Save_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        //------------------Load URL------------------
+        private void UI_Button_Load_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        ////------------------Helpers------------------
+
+
+    }
+}
