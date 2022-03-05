beefec0f13caa0a10f78307bc4fe79695ecfb96d
1201_cdisney1 <1201_cdisney1@319ddbfa-8900-c54b-9324-0d5e74af71d5>
2022-03-05 21:55:50 +0000

    added custom class to store image data, makes it easier to proccess later

---
+++ WebScraper_CDisney/CustomImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraper_CDisney
+{
+    class CustomImage
+    {
+        public string Url;      //Full url of image
+        public string Extension;//Extension of image
+        public string FileName; //Name of image
+
+        public CustomImage(string url)
+        {
+            Url = url;
+            FileName = url.Split('/').Last();
+            Extension = FileName.Split('.').Last();
+        }
+    }
+}
