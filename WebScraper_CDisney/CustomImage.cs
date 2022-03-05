43e37409d351ec386852def87ea6e4c3662007e4
1201_cdisney1 <1201_cdisney1@319ddbfa-8900-c54b-9324-0d5e74af71d5>
2022-03-05 22:08:11 +0000

    added some output of stats while finding links

--- WebScraper_CDisney/CustomImage.cs
+++ WebScraper_CDisney/CustomImage.cs
@@ -12,11 +12,26 @@ namespace WebScraper_CDisney
         public string Extension;//Extension of image
         public string FileName; //Name of image
 
+        /// <summary>
+        /// Cosntructor, takes url and parses necessary information
+        /// </summary>
+        /// <param name="url">url of image being processed</param>
         public CustomImage(string url)
         {
             Url = url;
             FileName = url.Split('/').Last();
             Extension = FileName.Split('.').Last();
+        }
+
+        /// <summary>
+        /// Override of equals to compare images by url
+        /// </summary>
+        /// <param name="obj">Object being comapred to</param>
+        /// <returns>Bool of equality based on url</returns>
+        public override bool Equals(object obj)
+        {
+            if (!(obj is CustomImage other)) throw new ArgumentException("Cannot compare different types");
+            return Url.Equals(other.Url);
         }
     }
 }
